3d666c39fc56b6ce6c1fa725224650ef03d4f303
Shanifos <Shanifoshee@gmail.com>
2019-09-28 17:26:39 -0400

    discounted items only

--- checkout.js
+++ checkout.js
@@ -1,13 +1,7 @@
-
 function selectSaleItems(items) {
-
   return items.discount > 0
 }
 
 const printSaleItems = (items) => items.filter(selectSaleItems)
-
-// function printSaleItems(items) {
-//   return items.filter(selectSaleItems)
-// }
 
 module.exports = printSaleItems
